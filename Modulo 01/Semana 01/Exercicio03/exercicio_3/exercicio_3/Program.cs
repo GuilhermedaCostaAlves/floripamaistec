be6c440cf3678ab097d94f28628d7ae7eca16d63
GuilhermedaCostaAlves <guicalves2011@gmail.com>
2023-02-07 21:25:39 -0300

    Exercícios feitos durante o curso

---
+++ Modulo 01/Semana 01/Exercicio03/exercicio_3/exercicio_3/Program.cs
@@ -0,0 +1,64 @@
+﻿int[] numeros = new int[10];
+int contador_impar = 0;
+int contador_par = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    int numero;
+
+    Console.WriteLine($"Digite o {i + 1} número: ");
+    numero = int.Parse(Console.ReadLine());
+    numeros[i] = numero;
+
+    if (numero % 2 == 0)
+    {
+        contador_par++;
+    }
+    else
+        contador_impar++;
+}
+int[] numeros_pares = new int[contador_par];
+int[] numeros_impares = new int[contador_impar];
+int contador_p = 0;
+int contador_i = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 == 0)
+    {
+        numeros_pares[contador_p] = numeros[i];
+        contador_p++;
+    }
+    else
+    {
+        numeros_impares[contador_i] = numeros[i];
+        contador_i++;
+    }
+}
+
+if (contador_p == 0)
+{
+    Console.WriteLine("\nNenhum número foi digitado na lista de pares.\n");
+}
+else
+{
+    Console.WriteLine($"\nA soma dos numeros pares é: {numeros_pares.Sum()}.\n");
+    Array.Sort(numeros_pares);
+    for (int i = 0; i < numeros_pares.Length; i++)
+    {
+        Console.WriteLine(numeros_pares[i]);
+    }
+}
+if (contador_i == 0)
+{
+    Console.WriteLine("\nNenhum número foi digitado na lista de impares.\n");
+}
+else
+{
+    Console.WriteLine($"\nA soma dos numeros impares é: {numeros_impares.Sum()}.\n");
+    Array.Sort(numeros_impares);
+    for (int i = 0; i < numeros_impares.Length; i++)
+    {
+        Console.WriteLine(numeros_impares[i]);
+    }
+}
---
+++ Modulo 01/Semana 01/Exercicio05/exercicio5/exercicio5/Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+int x = 1,y = 0, z = 0;
+
+Console.Write("Digite a quantidade de somas que deseja fazer: ");
+
+numero = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numero; i++)
+{
+    z = x + y;
+    Console.WriteLine(z);
+    y = x;
+    x = z;
+}
