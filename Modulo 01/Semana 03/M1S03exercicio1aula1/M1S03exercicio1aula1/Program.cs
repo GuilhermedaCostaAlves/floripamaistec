40bb4d0aaec16065fbd7c644b920f73ec75295a5
GuilhermedaCostaAlves <guicalves2011@gmail.com>
2023-02-13 21:52:26 -0300

    Enviando exercício 1 da semana 3 do modulo 1

---
+++ Modulo 01/Semana 03/M1S03exercicio1aula1/M1S03exercicio1aula1/Program.cs
@@ -0,0 +1,9 @@
+﻿using M1S03exercicio1aula1;
+
+Produto produto1 = new Produto("Calculadora", 5.75, 199);
+
+Produto produto2 = new Produto();
+
+Console.WriteLine($"O produto 1 tem nome: {produto1.nome}.\n\nValor: {produto1.preco}.\n\nQuantidade: {produto1.quantidade}.");
+
+Console.WriteLine($"\nO produto 2 tem nome: {produto2.nome}.\n\nValor: {produto2.preco}.\n\nQuantidade: {produto2.quantidade}.");
