93cfb1e9f6b174da974747358985d35ed9d5ded3
GuilhermedaCostaAlves <guicalves2011@gmail.com>
2023-05-16 19:14:00 -0300

    Exercícios realizados

---
+++ Modulo 02/M2S03Aula01/script.js
@@ -0,0 +1,22 @@
+//Variavel string
+
+var nome ="Guilherme Da Costa"
+
+//variavel number
+
+var idade = 25
+var salario = 1300.50
+
+//Bool
+
+var EstaDeRec = false
+
+//Objeto
+
+var aluno =
+{
+    nome :"Guilherme Da Costa",
+    idade : 22,
+    cidade: "Florianópolis",
+    estado: "Sc"
+}
