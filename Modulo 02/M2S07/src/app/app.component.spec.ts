d676e9a6420af860805b1ec2ffe9dbc01beaf73d
GuilhermedaCostaAlves <guicalves2011@gmail.com>
2023-07-08 14:32:22 -0300

    Commit dia 08 de julho

--- Modulo 02/M2S07/src/app/app.component.spec.ts
+++ Modulo 02/M2S07/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'm2S07'`, () => {
+  it(`should have as title 'M2S07'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('m2S07');
+    expect(app.title).toEqual('M2S07');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('m2S07 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('M2S07 app is running!');
   });
 });
