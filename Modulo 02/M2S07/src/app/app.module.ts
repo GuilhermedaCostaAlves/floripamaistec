6b3ddd9490abf3f153f1f14a2c53a8777b8099b2
GuilhermedaCostaAlves <guicalves2011@gmail.com>
2023-07-31 11:47:47 -0300

    commit dia 31

--- Modulo 02/M2S07/src/app/app.module.ts
+++ Modulo 02/M2S07/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    CalculadoraComponent,
   ],
   imports: [
     BrowserModule,
