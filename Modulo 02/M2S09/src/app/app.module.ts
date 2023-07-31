baff9f13ce475ef6c6e8172f6b24baa7ee128ce8
GuilhermedaCostaAlves <guicalves2011@gmail.com>
2023-07-31 15:26:18 -0300

    commit dia 31

--- Modulo 02/M2S09/src/app/app.module.ts
+++ Modulo 02/M2S09/src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './componnents/home/home.component';
-import { PulicComponent } from './componnents/pulic/pulic.component';
+import { FullComponent } from './layouts/full/full.component';
+import { NotFoundComponent } from './components/public/not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http'
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    PulicComponent,
+    FullComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
